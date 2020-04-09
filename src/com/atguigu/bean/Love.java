1467b829ae6da5a850aa6e1472fc9ba260817d97
lisiatguigu-git <yx985000>
2020-04-09 14:18:10 +0800

    dev分支

--- src/com/atguigu/bean/Love.java
+++ src/com/atguigu/bean/Love.java
@@ -4,5 +4,6 @@ public class Love {
 	public static void main(String[] args) {
 		System.out.println("我喜欢你，丽丝！--张三");
 		System.out.println("Are you handsome at guigu!--lisi22222");
+		System.out.println("创建了一个新的分支--丽丝");
 	}
 }
